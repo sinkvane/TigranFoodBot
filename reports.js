b2d9bd5bb00163a3d2e7bfdf8cfe86cb764cc199
sinkvane <sinkvadev@gmail.com>
2025-08-24 22:31:12 +0700

    test BIG

--- reports.js
+++ reports.js
@@ -11,9 +11,9 @@ export const POINTS = {
   "ул. Нурмагамбетова 102": { password: process.env.POINT_NURMAGAMBETOVA_102, tz: "KZ", type: "24h" },
 };
 const MANY = "15 19 * * *";
-const ONE = "31 20 * * *";
-const TWO = "31 20 * * *";
-const THREE = "31 20 * * *";
+const ONE = "33 20 * * *";
+const TWO = "33 20 * * *";
+const THREE = "33 20 * * *";
 const FIVE = "12 19 * * *";
 
 export const REMINDERS = [
