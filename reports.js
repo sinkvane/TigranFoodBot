eb8888a78a5fafe8904980439742823c3a916df5
sinkvane <sinkvadev@gmail.com>
2025-08-24 21:16:49 +0700

    buffer added

--- reports.js
+++ reports.js
@@ -11,9 +11,9 @@ export const POINTS = {
   "ул. Нурмагамбетова 102": { password: process.env.POINT_NURMAGAMBETOVA_102, tz: "KZ", type: "24h" },
 };
 const MANY = "15 19 * * *";
-const ONE = "11 19 * * *";
-const TWO = "12 19 * * *";
-const THREE = "13 19 * * *";
+const ONE = "17 19 * * *";
+const TWO = "17 19 * * *";
+const THREE = "17 19 * * *";
 const FIVE = "12 19 * * *";
 
 export const REMINDERS = [
